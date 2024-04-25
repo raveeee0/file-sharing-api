d11c3d65104c10ba4503136c640c3f1b7f57a446
raveeee <raveggigabriele0@gmail.com>
2024-04-26 00:03:09 +0200

    Added validation token middleware

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import AuthenticationException from '../exceptions/AuthenticationException';
+import passport from 'passport';
+import User from '../types/userInterface';
+
+function validateToken(req: express.Request, res: express.Response, next: express.NextFunction) {
+	if (!req.user) {
+		const authHeader = req.headers.authorization;
+		const token = authHeader && authHeader.split(' ')[1];
+		if (!token) {
+			next(new AuthenticationException('Token is missing'));
+		} else {
+			passport.authenticate('jwt', { session: false }, (err: Error, user: User) => {
+				if(err) 
+					next(err);
+				else if(!user)
+					next(new AuthenticationException('Invalid token'));
+				else {
+					req.user = user;
+					next();
+				}
+		})(req, res, next);
+	}
+	}
+};
+
+export default validateToken;
