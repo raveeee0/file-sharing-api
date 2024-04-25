58d18137a07ffb7ea9ee0e18961d3856fe5dd14c
raveeee <raveggigabriele0@gmail.com>
2024-04-26 00:00:55 +0200

    Created register router

---
+++ src/routes/register.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { body } from "express-validator";
+
+import * as userService from "../services/user";
+
+const router: express.Router = express.Router();
+
+router.post("/", [
+    body("name").isString().notEmpty(),
+    body("email").isEmail().notEmpty(),
+    body("password").isString().notEmpty(),
+    body("nickname").isString()
+], userService.createUser);
+
+export default router;
